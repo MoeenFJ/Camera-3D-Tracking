cd65991dc6193d4e8dfe5dfd8a5c07414ed926c5
MoeenFJ <moeenfarhadi@gmail.com>
2026-01-06 21:54:53 +0330

    before cam cal

--- Camera.py
+++ Camera.py
@@ -1,11 +1,12 @@
 import cv2
 import os
+import threading
 import numpy as np
 
 class Camera:
     
     ## TODO : Calculate this based on resolution
-    MIN_CONTOUR_AREA = 20
+    MIN_CONTOUR_AREA = 1000
     
     def __init__(self,name,path):
         self.name = ""
@@ -21,16 +22,28 @@ class Camera:
         self.inMatrix = None
         self.distortCoeff = None
         self.frame = None
-        self.frameReaderThread = None
+        self.savedFrame = None
         self.colorMap = {}
         
         self.name = name
         self.cap = cv2.VideoCapture(path,cv2.CAP_V4L2)
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.path = path
-  
+        
+        self.thread = threading.Thread(target=self._update, args=())
+        self.thread.daemon = True # Thread dies when main program exits
+        self.thread.start()
+        
+    def _update(self):
+        # Constantly grab frames to clear the buffer
+        while True:
+            ret, frame = self.cap.read()
+            if ret:
+                self.frame = frame
+                
     def readFrame(self):
-        ret, self.frame = self.cap.read()
-        return ret
+        self.savedFrame = self.frame.copy()
+
     
     def getBrightestContour(self):
         found = False
@@ -38,10 +51,10 @@ class Camera:
         cy = 0
         cont = None
         
-        #cv2.imwrite("CapturedCalData/debug_orig.png",self.frame)
+        #cv2.imwrite("CapturedCalData/debug_orig.png",self.savedFrame)
 
         
-        blur = cv2.medianBlur(self.frame, 5)
+        blur = cv2.medianBlur(self.savedFrame, 5)
         blur = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
         brightness = blur[:,:,2]
         #cv2.imwrite("CapturedCalData/debug_gray.png",brightness)
@@ -92,7 +105,7 @@ class Camera:
         
   
         ## TODO : USE HSV HERE INSTEAD OF RGB!!
-        blured_image = cv2.medianBlur(self.frame, 5)
+        blured_image = cv2.medianBlur(self.savedFrame, 5)
         
         color  = np.array(color, dtype = np.float32)
         target_unit = color / np.linalg.norm(color)
@@ -106,13 +119,15 @@ class Camera:
         gray_result = cv2.normalize(dot_product, None, 0, 255, cv2.NORM_MINMAX)
         gray_result = gray_result.astype(np.uint8)
 
-        ret, threshed = cv2.threshold(gray_result, 200, 255, cv2.THRESH_BINARY)
+        ret, threshed = cv2.threshold(gray_result, 150, 255, cv2.THRESH_BINARY)
 
         contours, hierarchy = cv2.findContours(
             threshed, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
         def _contourValue(cnt):
             return self.getAverageInContour(gray_result,cnt)[0]
+        
+        ## TODO : Currently the contourArea fucntion used can give wrong results specially if the contour is a line or self intesecting.
         contours = [cnt for cnt in contours if cv2.contourArea(cnt) > self.MIN_CONTOUR_AREA]
         
         ## TODO : CUTOFF HERE INSTEAD OF SORTING
@@ -120,8 +135,9 @@ class Camera:
 
         if len(contours) > 0:
             cont = contours[-1]
+            
             area = cv2.contourArea(cont)
-
+            ##print(f"{self.name} saw {color} with area {area}") 
             if area > 0:
                 found = True
                 M = cv2.moments(cont)
@@ -136,7 +152,7 @@ class Camera:
     def calibrateColor(self,color):
         f,c,cnt = self.getBrightestContour()
         assert f is not False, "A cam couldnt fine the node!"
-        observedColor = self.frame[c[1],c[0]]
+        observedColor = self.savedFrame[c[1],c[0]]
         if f:
             self.colorMap[color] = observedColor
 
@@ -150,7 +166,7 @@ class Camera:
         return self.getContourByColor(_color)
 
     def saveframe(self,loc):
-        cv2.imwrite(loc,self.frame)
+        cv2.imwrite(loc,self.savedFrame)
 
 class PS3EyeCamera(Camera):
     def normalSettings(self):
--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import glob
 import numpy as np
 import time
+from scipy.optimize import least_squares
 
 mtx = np.array([[ 554.2563,   0,          320],
  [  0,        554.2563,      240],
@@ -44,7 +45,7 @@ for camPath in glob.glob(CAMDIR):
     i += 1
     
 ## This will later be replaced with a button click saying "Done adding".
-while len(manager.nodes)<2:
+while len(manager.nodes)<1:
     pass
 
 
@@ -54,6 +55,7 @@ def RGBtoBGR(color):
 ## 1.It considers the average color in contour instead of the center
 ## 2.It considers the average color in a series of frames
 ## 3.It calculates a 3D margin of error plus 5% more error of error.
+time.sleep(2)
 def colorCalibration():
     calDataPath = Path("./CapturedCalData/")
     calDataPath.mkdir(parents=True,exist_ok=True)
@@ -69,8 +71,8 @@ def colorCalibration():
             print(f"Turn of {otherNode.color}, {otherNode.addr}")
         time.sleep(1)
         for cam in cameras:
-            for i in range(10):
-                cam.readFrame()
+            cam.readFrame()
+        for cam in cameras:
             col = RGBtoBGR(node.color)
             obsCol,cnt = cam.calibrateColor(col)
             print(f"Cam {cam.name} observed {node.color} as {obsCol}.")
@@ -90,26 +92,64 @@ colorCalibration()
 
 
 
-##Below is getContourByColor Test code
-while True:
+###Below is getContourByColor Test code
+#while True:
+#    for cam in cameras:
+#           cam.readFrame()
+#    for cam in cameras:
+#        img = cam.savedFrame
+#        for _,node in manager.nodes.items():
+#            col = RGBtoBGR(node.color)
+#            f, l, c = cam.getNodeInCamSpace(col)
+#            timg = img.copy()
+#            cv2.drawContours(timg, [c], 0, (0,255,0), 3)
+#            cv2.imshow(f"Cam {cam.name} found color {node.color}",timg)
+#            cv2.waitKey(1)
+#
+
+
+
+## Let's try template-less calibration first.
+## First let's gather some data.
+## Structure : In which test, for each camera, for each node, what location in cam space did we see the node at.
+## cam_calibration_data = {
+##    cam_index : {
+##        node_name : {
+##            cam_name : location_in_cam_space
+##        }
+##    }
+## }
+cam_calibration_data = []
+for i in range(5):
+    cam_calibration_data.append({})
     for cam in cameras:
         cam.readFrame()
-    for cam in cameras:
-        img = cam.frame
-        for _,node in manager.nodes.items():
+    for _,node in manager.nodes.items():
+        cam_calibration_data[i][node.name] = {}
+        for cam in cameras:
             col = RGBtoBGR(node.color)
             f, l, c = cam.getNodeInCamSpace(col)
-            timg = img.copy()
-            cv2.drawContours(timg, [c], 0, (0,255,0), 3)
-            cv2.imshow(f"Cam {cam.name} found color {node.color}",timg)
-            cv2.waitKey(1)
+            if not f:
+                continue
+            cam_calibration_data[i][node.name][cam.name] = l   
+    time.sleep(2)
+    
+    
+for dataFrame in cam_calibration_data:
+    for nodeName,nodeData in dataFrame.items():
+        print(f"Node {nodeName} : ")
+        for camName1,loc1 in nodeData.items():
+            print(f"    Cam {camName1} saw at {loc1}")
+            for camName2,loc2 in nodeData.items():
+                if camName1 is camName2:
+                    continue
+                print(f"        With Cam {camName2} at {loc2}")
+                
+
 
 ## For now, let's ignore calMethod and implement either one that heart desires.
 ## INCOMING UNTESTED CODE BELOW!!
 # while True:
-#     for cam in cameras:
-#         if cam.calibrated:
-#             cam.readFrame()
             
 #     results = []
 #     for cam1 in cameras:
